fa9692fae6ca5604a0b775226da9fa33c8f58ba3
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-03-18 21:55:25 +0300

    Update LinkedList.java

--- LinkedList1/src/main/java/LinkedList.java
+++ LinkedList1/src/main/java/LinkedList.java
@@ -135,3 +135,12 @@ public class LinkedList {
         System.out.println();
     }
 }
+
+class Node {
+    public int value;
+    public Node next;
+    public Node(int _value) {
+        value = _value;
+        next = null;
+    }
+}

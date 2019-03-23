b0daf2eca29bc9c1e246924dcbe182a3d0da33a9
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-03-23 19:29:10 +0300

    Add files via upload

---
+++ DynArray/src/main/java/DynArray.java
@@ -0,0 +1,98 @@
+import java.awt.peer.SystemTrayPeer;
+import java.lang.IndexOutOfBoundsException;
+
+public class DynArray<T>   
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+
+    public DynArray()
+    {
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        T[] temp = (T[]) new Object[capacity];
+
+        for (int i =0; i < count;i++) {         //сохраняем текущие значения массива во временный массив
+            temp[i] = array[i];
+        }
+
+        array =(T[]) new Object[new_capacity];  //создаем новый массив и записываем все текущие значения
+
+        for(int i = 0;i < count;i++){
+            array[i] = temp[i];
+        }
+
+        capacity = new_capacity;
+    }
+
+    public T getItem(int index)
+    {
+        if ((index < count) && (index >=0)) //проверка индекса
+            return array[index];
+        else
+            return  null;
+    }
+
+
+    public void append(T itm)
+    {
+        if (count == capacity){              //если массив заполнен
+            makeArray(capacity * 2);
+        }
+        array[count] = itm;
+        count++;
+    }
+
+
+    public void insert(T itm, int index) {
+        try {
+        if ((index > count) || (index < 0))  //проверка индекса
+        {
+            throw new IndexOutOfBoundsException("Попытка вставки элемента в недопустимую позицию");
+        }
+
+        if (count == capacity) {                            //если массив полностью заполнен
+            makeArray(capacity * 2);
+        }
+
+        for (int i = count; i > index; i--) {              //смещение элементов
+            array[i] = array[i - 1];
+        }
+
+        array[index] = itm;                                //вставка
+        count++;
+    }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void remove(int index) {
+        try {
+        if ((index >= count) || (index < 0))               //проверка индекса
+        {
+            throw new IndexOutOfBoundsException("Попытка удаления элемента в недопустимой позиции");
+        }
+
+        for (int i = index; i < (count - 1); i++) {        //удаление
+            array[i] = array[i + 1];
+        }
+        count--;
+
+        if (2 * count < capacity)
+            if (capacity > 32)
+                makeArray(((capacity * 2) / 3));
+            else
+                makeArray(16);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ DynArray/src/test/java/DynArrayTest.java
@@ -0,0 +1,74 @@
+import static org.junit.Assert.*;
+
+public class DynArrayTest {
+
+    @org.junit.Test
+    public void getItem() {
+        DynArray<Integer> arr = new DynArray<Integer>();
+        Integer a = 2;
+        arr.append(1);
+        arr.append(a);
+        assertEquals(a, arr.getItem(1));
+    }
+
+    @org.junit.Test
+    public void append() {
+        DynArray<Integer> arr = new DynArray();
+        for (int i = 0; i < 20; i++){
+            Integer a = i;
+            arr.append(a);
+        }
+        assertEquals(32,arr.capacity);
+    }
+
+    @org.junit.Test
+    public void insert() {
+        DynArray<Integer> arr = new DynArray();   //вставка элемента когда, массив не заполнен полностью
+        for (int i = 10; i < 20; i++){
+            Integer a = i;
+            arr.append(a);
+        }
+        arr.insert(9,0);
+        assertEquals((Integer)9,arr.getItem(0));
+        assertEquals((Integer)10,arr.getItem(1));
+        assertEquals(16,arr.capacity);
+
+        arr.insert(21,11);                        //вставка последним элементом
+        assertEquals((Integer) 21, arr.getItem(11));
+
+        arr.insert(23,14);                        //вставка в недопустимую позицию
+
+        for(int i = 22; i < 26;i++){
+            arr.append(i);
+        }
+
+        arr.insert(8,0);                         //вставка, когда массив переполнен
+        assertEquals(32,arr.capacity);
+
+    }
+
+    @org.junit.Test
+    public void remove() {
+        DynArray<Integer> arr = new DynArray();  //удаление элемента когда, массив не заполнен полностью
+        for (int i = 5; i < 20; i++){
+            Integer a = i;
+            arr.append(a);
+        }
+        arr.remove(0);
+        assertEquals((Integer)6,arr.getItem(0));
+        assertEquals(16,arr.capacity);
+
+       arr.remove(16);                        //удаление несуществующего индекса
+
+        arr.append(21);                             //удаление, когда массив заполнен
+        arr.append(22);
+        arr.append(23);
+        assertEquals(32,arr.capacity);
+
+        arr.remove(0);
+        arr.remove(1);
+        assertEquals(16,arr.capacity);
+
+
+    }
+}

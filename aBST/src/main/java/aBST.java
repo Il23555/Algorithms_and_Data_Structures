592e384c01007b5186b950b657a3e9b9ae3b2a6c
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-04-27 17:31:37 +0300

    Add files via upload

---
+++ aBST/src/main/java/aBST.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class aBST
+{
+    public Integer Tree []; // массив ключей
+
+    public aBST(int depth)
+    {
+        // правильно рассчитайте размер массива для дерева глубины depth:
+        int tree_size = 0;
+        int t = 1;
+        for (int i = 0; i < depth ; i++) {
+            tree_size = tree_size + t;
+            t = t*2;
+        }
+        Tree = new Integer[ tree_size ];
+        for(int i=0; i<tree_size; i++) Tree[i] = null;
+    }
+
+    public Integer FindKeyIndex(int key)
+    {
+        int index = 0;
+        while (Tree.length > index){
+            if (Tree[index] == null){
+                return key*(-1);
+            }
+            if (Tree[index] == key)
+                return index;
+            if (Tree[index] > key)
+                index = 2*index+1;
+            else
+            if (Tree[index] < key)
+                index = 2 * index + 2;
+        }
+        return null; // не найден
+    }
+
+    public int AddKey(int key)
+    {
+        int index = 0;
+        while (Tree.length > index){
+            if (Tree[index] == null){
+                Tree[index] = key;
+                return index;
+            }
+            if (Tree[index] == key)
+                return index;
+            if (Tree[index] > key)
+                index = 2*index+1;
+            else
+                if (Tree[index] < key)
+                    index = 2 * index + 2;
+
+        }
+        return -1;
+        // индекс добавленного/существующего ключа или -1 если не удалось
+    }
+
+}
---
+++ aBST/src/test/java/aBSTTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+public class aBSTTest {
+
+    @org.junit.Test
+    public void findKeyIndex() {
+        aBST tree = new aBST(3);
+        assertEquals((Integer)(-9),tree.FindKeyIndex(9));
+        assertEquals(0,tree.AddKey(9));
+        tree.AddKey(6);
+        tree.AddKey(14);
+        tree.AddKey(10);
+        tree.AddKey(20);
+        tree.AddKey(3);
+        tree.AddKey(7);
+        tree.AddKey(1);
+        assertEquals((Integer)0,tree.FindKeyIndex(9));
+        assertEquals((Integer)1,tree.FindKeyIndex(6));
+        assertEquals((Integer)2,tree.FindKeyIndex(14));
+        assertEquals(null,tree.FindKeyIndex(1));
+    }
+
+    @org.junit.Test
+    public void addKey() {
+        aBST tree = new aBST(2);
+        assertEquals(0,tree.AddKey(9));
+        assertEquals(1,tree.AddKey(6));
+        assertEquals(2,tree.AddKey(14));
+        assertEquals(-1,tree.AddKey(10));
+
+        for (int i = 0; i < tree.Tree.length; i++) {
+            System.out.print(tree.Tree[i] + "  ");
+        }
+    }
+}
